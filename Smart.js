562450455abba1ed5780fce03528dc4c1ff14792
srinjoychinargoswami <66094180+srinjoychinargoswami@users.noreply.github.com>
2021-12-21 12:49:22 -0500

    Updated Smart.Js code
    Updated Smart.js code to most recent version published 16 days ago. More edits, from Dr. Goswami are needed. Control Function and F12 keys function disabled script is also added here. You will need to add the Block inspect script.

--- Smart.js
+++ Smart.js
@@ -1,5 +1,4 @@
 <script> 
-
 function genomic () { 
 var genomic = Number(document.getElementById("Genomic").value);
 var subgenomic = Number(document.getElementById("Subgenomic").value);
@@ -8,28 +7,35 @@ var delta_delta = ((subgenomic - genomic) - RPN_ct)
 var fold = Math.pow(2, -delta_delta )
 
 if (RPN_ct >= 36) { 
-        document.getElementById("result").innerHTML =  "Result:" +'Invalid run due to RPN Ct values';
+        document.getElementById("result").innerHTML =  "Result:" +'Invalid run due to RNAse P Ct values';
 }  
 if (RPN_ct < 36) { 
-   if (genomic <= 21) { 
+   if (genomic <= 23) { 
         document.getElementById("result").innerHTML = "Result: " +  'Sample is Positive for actively replicating SARS-CoV-2 Virus';
-} else if (genomic > 40) { 
-  document.getElementById("result").innerHTML = "Result: " + 'Invalid Run due to Genomic Ct value';
+} else if (genomic >= 35 ) { 
+  document.getElementById("result").innerHTML = "Result: " + 'Sample is Negative for SARS-CoV-2 Virus';
 } else if (genomic >= 36 && genomic <= 40) { 
         document.getElementById("result").innerHTML = "Result:" + 'Sample is Negative for actively replicating SARS-CoV-2 Virus';
 } else if (genomic > 21 && genomic < 36) {
-    if (fold >= 2444508) { 
+    if (fold >= 1751090) { 
         document.getElementById('result').innerHTML = "Result: " + 'Sample is Positive for actively replicating SARS-CoV-2 Virus'; 
         
-} else if (fold < 2444508) { 
+} else if (fold < 1751090) { 
         document.getElementById('result').innerHTML = "Result:" + 'Sample is Negative for actively replicating SARS-CoV-2 Virus';
     } 
 }
-
   }      
 }
     
-
-
-
-</script> 
+</script>
+<script>
+document.addEventListener("keydown", function(e){
+  // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+  // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+  // THIS WILL ONLY DISABLE CONTROL AND F12
+  if (e.ctrlKey || e.keyCode==123) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+});
+</script>
